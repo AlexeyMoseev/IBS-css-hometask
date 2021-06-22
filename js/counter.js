22c58ae792f844f6e4cc04b073d4be6e0bfac38d
AlexeyMoseev <blackcatalexis123@gmail.com>
2021-06-22 23:35:46 +0500

    1. Исправил все не тождественные сравнения 2. Везде добавил символы конца строки (символ ;) 3. Убрал id, который не нес никакой смысловой и информационной нагрузки 4. Убрал null в методе xhr.send() 5. Сделал шаблонные литералы, в местах где идет обращение к серверу 6. В местах где идет сообщение об ошибке написал console.error(e) вместо console.log(e) 7. Исправил let на const там где это было необходимо

--- js/counter.js
+++ js/counter.js
@@ -1,12 +1,12 @@
-let countInput = document.getElementsByClassName('count')[0];
-let remove = document.getElementsByClassName('remove')[0];
-let add = document.getElementsByClassName('add')[0];
-let units = countInput.value.replace(/\d/g, '');
-add.onclick = function(){
-    countInput.value = parseInt(countInput.value)+1+units;
+const countInput = document.getElementsByClassName('count')[0];
+const remove = document.getElementsByClassName('remove')[0];
+const add = document.getElementsByClassName('add')[0];
+const units = countInput.value.replace(/\d/g, '');
+add.onclick = function () {
+	countInput.value = parseInt(countInput.value) + 1 + units;
 };
-remove.onclick = function(){
-    if(parseInt(countInput.value) > 1) {
-        countInput.value = parseInt(countInput.value)-1+units;
-    }
+remove.onclick = function () {
+	if (parseInt(countInput.value) > 1) {
+		countInput.value = parseInt(countInput.value) - 1 + units;
+	}
 };
